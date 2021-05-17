48f1360db8b75e7b597b69406a2922293f6aeb64
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2021-05-17 16:18:46 -0300

    chore: Validation done and start user's middleware

--- backend/api/user.js
+++ backend/api/user.js
@@ -1,6 +1,31 @@
+const bcrypt = require('bcrypt-nodejs')
+
 module.exports = app => {
-    const save = (req, res) => {
-        res.send('User save')
+    const { existsOrError, notExistsOrError, equalsOrError } = app.api.valitaion
+
+    const encryptPassword = password => {
+        const salt = bcrypt.genSaltSync(10)
+        return bcrypt.hashSync(password, salt)
+    }
+
+    const save = async (req, res) => {
+        const user = {...req.body}
+        if(req.params.id) user.id = req.params.id
+
+        try{
+            existsOrError(user.name, 'Nome não informado')
+            existsOrError(user.email, 'E-mail não informado')
+            existsOrError(user.password, 'Senha não informada')
+            existsOrError(user.confirmPassword, 'Confirmação de Senha inválida')
+            equalsOrError(user.password, user.confirmPassword,
+                'Senhas não conferem')
+            
+            const userFromDB = await app.db('users')
+                .where({ email: user.email }).first()
+            if(!user.id){
+                notExistsOrError(userFromDB, 'Usuário já cadastrado')
+            }
+        }
     }
 
     return { save }
