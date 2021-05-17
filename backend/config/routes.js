337eef4f5cf21f91666ebeed92b591f98f82f088
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2021-05-17 17:03:16 -0300

    chore: User's routes created

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,4 +1,8 @@
 module.exports = app => {
     app.route('/users')
         .post(app.api.user.save)
+        .get(app.api.user.get)
+    app.route('/users')
+        .put(app.api.user.save)
+        .get(app.api.user.getById)
 }
