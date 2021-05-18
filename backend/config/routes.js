fed324ec0d793c9a6b7a9975c36a8a91cdcb01d0
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2021-05-18 14:32:53 -0300

    chore: Categories route done

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -2,12 +2,19 @@ module.exports = app => {
     app.route('/users')
         .post(app.api.user.save)
         .get(app.api.user.get)
+
     app.route('/users/:id')
         .put(app.api.user.save)
         .get(app.api.user.getById)
+
     app.route('/categories')
         .get(app.api.category.get)
         .post(app.api.category.save)
+
+    // Cuidado com a ordem!
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
+
     app.route('/categories/:id')
         .get(app.api.category.getById)
         .put(app.api.category.save)
