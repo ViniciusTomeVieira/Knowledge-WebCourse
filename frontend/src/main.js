68db296b87c5a0faa1ad0d8b2acf4eabdfe7519f
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2021-05-19 16:08:26 -0300

    feat: Create dashboard stats vue reading mongoDB data

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,8 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkphaWxzb24gTWVuZGVzIiwiZW1haWwiOiJqYWlsc3VAMTIzLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2MjE0NTA5NzgsImV4cCI6MTYyMTcxMDE3OH0.VWfro7NGdkHzcyS82amhNZv_Frhzh_PQMOmTw8MDvmM'
+
 new Vue({
   store,
   router,
