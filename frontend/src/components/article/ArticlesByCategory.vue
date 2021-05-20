e05d54e07acbb9eaa62db462c355b24851ec9a9c
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2021-05-20 15:45:57 -0300

    feat: Articles view list working

--- frontend/src/components/article/ArticlesByCategory.vue
+++ frontend/src/components/article/ArticlesByCategory.vue
@@ -4,7 +4,7 @@
             :main="category.name" sub="Categoria" />
         <ul>
             <li v-for="article in articles" :key="article.id">
-                <!-- <ArticleItem :article="article" /> -->
+                <ArticleItem :article="article" />
             </li>
         </ul>
         <div class="load-more">
@@ -19,11 +19,11 @@
 import { baseApiUrl } from '@/global'
 import axios from 'axios'
 import PageTitle from '../template/PageTitle'
-//import ArticleItem from './ArticleItem'
+import ArticleItem from './ArticleItem'
 
 export default {
     name: 'ArticlesByCategory',
-    components: { PageTitle },
+    components: { PageTitle, ArticleItem },
     data: function() {
         return {
             category: {},
