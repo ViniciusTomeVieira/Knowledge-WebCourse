0581a53d8c722002010754438d916e366ed2185b
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2021-05-20 14:51:09 -0300

    feat: Create article admin page

--- backend/api/article.js
+++ backend/api/article.js
@@ -10,8 +10,8 @@ module.exports = app => {
         try {
             existsOrError(article.name, 'Nome não informado')
             existsOrError(article.description, 'Descrição não informada')
-            existsOrError(article.categoryId, 'Categoria não informada')
-            existsOrError(article.userID, 'Autor não informado')
+            existsOrError(article.categoryID, 'Categoria não informada')
+            existsOrError(article.userId, 'Autor não informado')
             existsOrError(article.content, 'Conteúdo não informado')
         } catch(msg) {
             res.status(400).send(msg)
