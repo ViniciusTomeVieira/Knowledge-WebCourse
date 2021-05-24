06b49c030a308e05d290f91d8a3d1d1536306511
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2021-05-24 13:47:35 -0300

    chore: Start validating auth token

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,8 @@
 </template>
 
 <script>
+import axios from "axios"
+import { baseApiUrl, userKey } from '@/global'
 import { mapState } from 'vuex'
 import Header from '@/components/template/Header'
 import Menu from '@/components/template/Menu'
@@ -17,7 +19,45 @@ import Footer from '@/components/template/Footer'
 export default {
 	name: "App",
 	components: { Header, Menu, Content, Footer},
-	computed: mapState(['isMenuVisible', 'user'])
+	computed: mapState(['isMenuVisible', 'user']),
+	data: function() {
+		return {
+			validatingToken: true
+		}
+	},
+	methods: {
+		async validateToken() {
+			this.validatingToken = true
+
+			const json = localStorage.getItem(userKey)
+			const userData = JSON.parse(json)
+			this.$store.commit('setUser', null)
+
+			if(!userData) {
+				this.validatingToken = false
+				this.$router.push({ name: 'auth' })
+				return
+			}
+
+			const res = await axios.post(`${baseApiUrl}/validateToken`, userData)
+
+			if (res.data) {
+				this.$store.commit('setUser', userData)
+				
+				if(this.$mq === 'xs' || this.$mq === 'sm') {
+					this.$store.commit('toggleMenu', false)
+				}
+			} else {
+				localStorage.removeItem(userKey)
+				this.$router.push({ name: 'auth' })
+			}
+
+			this.validatingToken = false
+		},
+		created() {
+		this.validateToken()
+	}
+	}
 }
 </script>
 
